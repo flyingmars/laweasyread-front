4c06cb45c5c377fb6c743305dcc4ddcdd61c0463
kong0107 <kong0107@gmail.com>
2013-08-25 13:27:29 +0800

    Resolves #7
    Though not auto-commit to another branch yet. See #7 itself for more
    information.

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,44 @@ module.exports = function(grunt) {
       chrome: {
         src: '<%= compress.chrome.options.archive %>',
         dest: 'dist/<%= pkg.name %>.crx'
+      },
+      firefox: {
+        src: [
+          "icon.png",
+          "main.css",
+          "parseInt.js",
+          "pcodes.js",
+          "aliases.js",
+          "lyIDs.js",
+          "courts.js",
+          "LER.js"
+        ],
+        dest: 'data/'
+      }
+    },
+    shell: {
+      options: {
+        stdout: true,
+        stderr: true,
+        failOnError: true
+      },
+      firefox: {
+        command: (
+          (require('os').type().indexOf("Windows") == 0)
+          ? [
+            '"e:\\Program Files\\addon-sdk-1.14\\bin\\activate"', /* !!EDIT!! */
+            'cfx xpi',
+            'move <%= pkg.name %>.xpi dist',
+            'copy dist\\<%= pkg.name %>.xpi dist\\<%= pkg.version %>\\<%= pkg.name %>-<%= pkg.version %>.xpi',
+            'rmdir data /S /Q' /* !!CHECK!! */
+          ] : [
+            '"/**/addon-sdk*/bin/activate"', /* !!EDIT!! */
+            'cfx xpi',
+            'mv <%= pkg.name %>.xpi dist',
+            'cp dist/<%= pkg.name %>.xpi dist/<%= pkg.version %>/<%= pkg.name %>-<%= pkg.version %>.xpi',
+            'rm -rf data' /* !!CHECK!! */
+          ]
+        ).join('&&')
       }
     }
   });
@@ -64,6 +102,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('default', ['concat', 'uglify', 'compress', 'copy']);
+  grunt.registerTask('default', ['concat', 'uglify', 'compress', 'copy', 'shell']);
 };
