dede97c7cb76d5097c528d7211509bb625ba4eea
kong0107 <kong0107@gmail.com>
2018-11-09 03:21:39 +0800

    準備開發，只先留一定會需要的檔案；換logo

--- embedded2.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var orig = window.onload ? window.onload : function(){};
-    return function() {
-        var result = orig.apply(this, arguments);
-        LER.parse(document.body);
-        return result;
-    };
-}();
