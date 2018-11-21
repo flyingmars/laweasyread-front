5fd6b1e9ad54fce981c30339b833a98ff8dcf2dd
kong0107 <kong0107@gmail.com>
2018-11-22 05:21:16 +0800

    add links and forms to popup.html

--- content_scripts/lis.ly.js
+++ content_scripts/lis.ly.js
@@ -92,3 +92,14 @@ if(lawName) LER.loadLaws.then(() => {
     LER.defaultLaw = LER.getLaw({name: lawName});
 });
 }
+
+
+/**
+ * 搭配 `popup.html` 的表單以強制啟動立法院法律系統的搜尋功能。
+ */
+if(!location.search) {
+    const input = document.getElementsByName("_1_5_T")[0];
+    if(input && input.value)
+        document.getElementsByName("_IMG_檢索")[0].click();
+
+}
